c33bc4ed6bad284969d47bc48a9b275415f0f0a0
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2025-04-19 17:56:02 +0300

    Added disappearance/appearance of the header when scrolling the page

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-console.log("hi")
-
 document.addEventListener("DOMContentLoaded", function(){
     input = document.querySelector(".header_text-field")
     inputIcon = document.querySelector(".header_search")
     inputIcon.addEventListener("click", () => {
         input.classList.toggle("active")
     })
+
+    header = document.querySelector(".header")
+    const width = document.documentElement.clientWidth
+    let headerHeight = header.clientHeight
+
+    let top = 0
+    if (width > 1024) {
+        document.addEventListener("scroll", () => {
+            if (window.pageYOffset < top) {
+                header.style.top = 0
+            }
+            else {
+                if (window.pageYOffset > 200){
+                    header.style.top = "-" + headerHeight + "px"
+                }
+            }
+            top = window.pageYOffset
+        })
+    }
 })
